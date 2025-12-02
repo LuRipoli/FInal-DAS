ca5884e01fabac2b1436f3a889733b9e3a620fb0
Saimon <splazavidal@gmail.com>
2025-12-02 16:17:59 -0300

    Controladora de Producto y de Consulta

--- Controladora/ControladoraProductos.cs
+++ Controladora/ControladoraProductos.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades; 
 
 namespace Controladora
 {
@@ -21,5 +22,57 @@ namespace Controladora
         }
 
         private ControladoraProductos() { }
+
+
+        public IReadOnlyCollection<Entidades.Producto> ObtenerProducto()        //Para obtener todos los productos
+        {
+            return repositorio.ObtenerProducto();
+        }
+
+        public Producto? ObtenerProductoPorID(int idProducto)                   //Para obtener un producto pr ID
+        {
+            if (idProducto <= 0)
+            {
+                return null; 
+            }
+            return repositorio.ObtenerProductoPorID(idProducto); 
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            if (producto == null)
+            {
+                throw new ArgumentNullException(nameof(producto));
+                repositorio.AgregarProducto(producto); 
+            }
+        }
+
+        public void ModificarProducto(Producto producto)
+        {
+            if (producto == null)
+            {
+                throw new ArgumentNullException(nameof(producto));
+            }
+            repositorio.ModificarProducto(producto); 
+        }
+
+        public bool EliminarProducto( int idProducto)
+        {
+            if (idProducto <= 0)
+            {
+                return false; 
+
+            }
+
+            var producto =  repositorio.ObtenerProductoPorID(idProducto); 
+            
+            if (producto == null)
+            {
+                return false; 
+            }
+
+            repositorio.EliminarProducto(producto);
+            return true; 
+        }
     }
 }
