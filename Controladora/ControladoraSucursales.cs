22abf39b7dcb9aa40eed7e93ab0089f66ccca98d
Mateo <matumelgar15@gmail.com>
2025-12-02 17:21:56 -0300

    commit

--- Controladora/ControladoraSucursales.cs
+++ Controladora/ControladoraSucursales.cs
@@ -1,4 +1,5 @@
-﻿using Modelo;
+﻿using Entidades;
+using Modelo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,53 @@ namespace Controladora
         }
 
         private ControladoraSucursales() { }
+
+
+        public IReadOnlyCollection<Entidades.Sucursal> ObtenerSucursales()        //Para obtener todas las sucursales
+        {
+            return repositorio.ObtenerSucursales();
+        }
+       
+        public Sucursal? ObtenerSucursalPorID(int idSucursal)                   //Para obtener una sucursal por ID
+        {
+            if (idSucursal <= 0)
+            {
+                return null; 
+            }
+            return repositorio.ObtenerSucursalPorId(idSucursal);
+        }
+
+        public void AgregarSucursal(Sucursal sucursal)
+        {
+            if (sucursal == null)
+            {
+                throw new ArgumentNullException(nameof(sucursal));
+                repositorio.AgregarSucursal(sucursal); 
+            }
+        }
+
+        public void ModificarSucursal(Sucursal sucursal)
+        {
+            if (sucursal == null)
+            {
+                throw new ArgumentNullException(nameof(sucursal));
+            }
+            repositorio.ModificarSucursal(sucursal);
+        }
+        public bool EliminarSucursal(int idSucursal)
+        {
+            if (idSucursal <= 0)
+            {
+                return false;
+            }
+            var sucursal = ObtenerSucursalPorID(idSucursal);
+            if (sucursal == null)
+            {
+                return false;
+            }
+            repositorio.EliminarSucursal(sucursal);
+            return true;
+        }
     }
 }
 
--- Controladora/ControladoraVentas.cs
+++ Controladora/ControladoraVentas.cs
@@ -1,4 +1,5 @@
-﻿using Modelo;
+﻿using Entidades;
+using Modelo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,5 +22,53 @@ namespace Controladora
         }
 
         private ControladoraVentas() { }
+
+
+        public IReadOnlyCollection<Entidades.Venta> ObtenerVentas()        //Para obtener todas las ventas
+        {
+            return repositorio.ObtenerVentas();
+        }
+
+        public Venta? ObtenerVentaPorID(int idVenta)                   //Para obtener una venta por ID
+        {
+            if (idVenta <= 0)
+            {
+                return null;
+            }
+            return repositorio.ObtenerVentaPorId(idVenta);
+        }
+
+        public void AgregarVenta(Venta venta)
+        {
+            if (venta == null)
+            {
+                throw new ArgumentNullException(nameof(venta));
+                repositorio.AgregarVenta(venta);
+            }
+        }
+
+        public void ModificarVenta(Venta venta)
+        {
+            if (venta == null)
+            {
+                throw new ArgumentNullException(nameof(venta));
+            }
+            repositorio.ModificarVenta(venta);
+        }
+
+        public bool EliminarVenta(int idVenta)
+        {
+            if (idVenta <= 0)
+            {
+                return false;
+            }
+            var venta = repositorio.ObtenerVentaPorId(idVenta);
+            if (venta == null)
+            {
+                return false;
+            }
+            repositorio.EliminarVenta(venta);
+            return true;
+        }
     }
 }
